470fc77d8aac747970f1214e00d1a616e0ab9031
Unknown <novik.yuliya.1@gmail.com>
2017-08-20 17:29:46 +0300

    Add functionality

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 ((login, password) => {
+
+    var Horseman = require("node-horseman");
+    const URL = "https://dev.by/login";
+
+    function Login(login, password) {
+        new Horseman()
+            .open(URL)
+            .waitForSelector("input[name='user[login]']", { timeout: 60000 })
+            .select("input[name='user[login]']", login)
+            .select("input[name='user[password]']", password)
+            .click("input[name='commit']")
+            .waitForNextPage({ timeout: 60000 })
+            .screenshot("1.jpg")
+            .close();
+    }
+
+    Login(login, password)
+
 })(process.argv[2], process.argv[3]);
